e50461757c4f8bd5be269ce1bd4c9f6a5b91d13a
Rod Holland <rholland@structurelabs.com>
2020-11-15 10:25:36 -0800

    Create rc_example1.py

---
+++ rc_example1.py
@@ -0,0 +1,19 @@
+import rivtcalc.rc_lib as rc
+
+rc.I(""" text 1
+    this is an item
+    
+    abcde
+    
+"""
+    
+rc.V(""" text 2
+    Some equations
+
+    a = 3 + 2
+    
+    b = 4 +5.5
+    
+"""
+    
+    
